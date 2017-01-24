c2e2839c39653c5c0e65e5f2d44f6a1c3d13fc5f
MaciekTalaska <maciek.talaska@gmail.com>
2017-01-24 18:25:33 +0100

    fish shell: function for storing abbreviations

---
+++ .config/fish/functions/customize_abbreviations.fish
@@ -0,0 +1,18 @@
+function customize_abbreviations
+  # inspired by: https://github.com/fish-shell/fish-shell/issues/731#issuecomment-244457493
+  if not set -q MaciekTalaskaFishConfig 
+    abbr -a ga git add
+    abbr -a gp git push
+    abbr -a gc git commit
+    abbr -a gs git status
+    abbr -a gl git ls
+    abbr -a gd git diff
+    abbr -a gau git add -u
+    abbr -a gcm git commit -m
+    abbr -a gca git commit -a -m
+    abbr -a gbl git branch --list
+    abbr -a gsl git stash list
+    abbr -a gdc git diff --cached
+    set -U MaciekTalaskaFishConfig
+  end
+end
