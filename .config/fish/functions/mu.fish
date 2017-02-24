e647f2c91fa276fd456867328bf939be5882908c
MaciekTalaska <maciek.talaska@gmail.com>
2017-02-24 22:34:10 +0100

    fish: formatting...

--- .config/fish/functions/mu.fish
+++ .config/fish/functions/mu.fish
@@ -1,6 +1,6 @@
 function mu
-	sudo apt-get update
-sudo apt-get dist-upgrade 
-sudo apt-get autoremove 
-sudo apt-get autoclean 
+  sudo apt-get update
+  sudo apt-get dist-upgrade 
+  sudo apt-get autoremove 
+  sudo apt-get autoclean 
 end
