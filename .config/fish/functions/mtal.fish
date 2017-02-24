d7d29ef01d3e01f916565391459ef0751d6b14e5
MaciekTalaska <maciek.talaska@gmail.com>
2017-02-24 22:33:07 +0100

    fish: refactoring, mfunced added

--- .config/fish/functions/mtal.fish
+++ .config/fish/functions/mtal.fish
@@ -1,6 +1,6 @@
 # My own fish customization functions
 
-function configs_backup -d "copy current (used configuraton) into ~/projects/dotfiles" 
+function mt_configs_backup -d "copy current (used configuraton) into ~/projects/dotfiles" 
   pushd .
   rsync -avR .config/fish/functions/ ~/projects/dotfiles/
   rsync -avR .config/fish/config.fish ~/projects/dotfiles/
@@ -16,22 +16,36 @@ function configs_backup -d "copy current (used configuraton) into ~/projects/dot
   popd
 end
 
-function configs_apply_all -d "copy all configs from ~/projects/dotfiles into ~"
+function mt_configs_apply_all -d "copy all configs from ~/projects/dotfiles into ~"
   pushd .
   cd ~
   rsync -avR ~/projects/dotfiles ~/
-  __mtal_remove_fishd
+  __mt_remove_fishd
   popd
 end
 
-function configs_apply_fish -d "copy only fish configuration from ~/project/dotfiles into ~"
+function mt_configs_apply_fish -d "copy only fish configuration from ~/project/dotfiles into ~"
   pushd .
   cd ~
   rsync -av ~/projects/dotfiles/.config/fish ~/.config
-  __mtal_remove_fishd
+  __mt_remove_fishd
   popd
 end
 
-function __mtal_remove_fishd
+function __mt_remove_fishd
   rm ~/.config/fish/fishd.*
+end
+
+function __mt_erase_editor
+  set EDITOR ""
+end
+
+function __mt_restore_editor
+  set EDITOR $__editor  
+end
+
+function mfunced -d "same as funced, but uses only internal fish editor"
+  __mt_erase_editor
+  funced $argv
+  __mt_restore_editor
 end
