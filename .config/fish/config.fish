6a025cc5eb82e9cf7357cfaa489642fc91c5b8bf
MaciekTalaska <maciek.talaska@gmail.com>
2018-03-20 08:47:33 +0100

    do not use powerline - use my own prompt instead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,17 @@
 set fish_greeting ''
 set -x LANG en_US.UTF-8
 
+source $HOME/.config/fish/functions/mt_prompt.fish
+
 function fish_prompt
-    ~/configuration/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null --colorize-hostname --cwd-mode plain
+#    ~/configuration/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null --colorize-hostname --cwd-mode plain
+  __mt_simple  
 end
 
 source $HOME/.config/fish/functions/mt_tools.fish
 mt_customize_abbreviations
 
-source ~/.asdf/asdf.fish
+#source ~/.asdf/asdf.fish
 
 set __editor vim
 set -Ux EDITOR $__editor 
