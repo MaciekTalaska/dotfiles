d9d96631113df6ca89d56fcae5d53fc5c9f58749
MaciekTalaska <maciek.talaska@gmail.com>
2018-03-20 08:59:31 +0100

    script to automate installation of dependencies after cloning repo

---
+++ first-install.sh
@@ -0,0 +1,10 @@
+cd
+mkdir configuration
+cd configuration
+git clone https://github.com/powerline/fonts
+cd fonts
+./install.sh
+cd ..
+
+git clone https://github.com/michaeldfallen/git-radar
+cd ..
