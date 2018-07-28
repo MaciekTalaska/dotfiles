f7aa25bd3f9562ef3ef9cf639e80eae69a369078
MaciekTalaska <maciek.talaska@gmail.com>
2018-07-28 18:09:55 +0200

    improved p4merge install script

--- p4vinstall.sh
+++ p4vinstall.sh
@@ -3,38 +3,61 @@
 # Perforce download site:
 # http://filehost.perforce.com/perforce/
 
-pushd -n $(pwd)
+install() {
+    pushd -n $(pwd)
 
-if [ -d ~/apps/p4v-baq ]; then
-    rm ~/apps/p4v-baq -rf
-fi
+    local fullpath=~/apps/p4v
+    local baqpath=~/apps/p4v-baq
+    local appsdirectory=~/apps
 
-# backup old p4v installation (if exists)
-if [ -d ~/apps/p4v ]; then
-    mv ~/apps/p4v ~/apps/p4v-baq
-fi
+    create_destination_directory
 
-if [ ! -d ~/apps ]; then
-    mkdir ~/apps/p4v
-fi
+    cd ~/apps/p4v
 
-if [ ! -d ~/apps/p4v ]; then
-    mkdir ~/apps/p4v
-fi
+    download_p4merge_archive
+    
+    extract_p4merge
 
-cd ~/apps/p4v
+    create_symlink
+    popd
+}
 
-# TODO: instead of hard-coded url for a specific version
-# it would be best to get info on the newest version availablev
-wget -O p4v.tgz http://filehost.perforce.com/perforce/r18.2/bin.linux26x86_64/p4v.tgz
+create_destination_directory() {
+    if [ -d ~/apps/p4v-baq ]; then
+        rm ~/apps/p4v-baq -rf
+    fi
 
-tar -xf p4v.tgz --strip 1
+    # backup old p4v installation (if exists)
+    if [ -d ~/apps/p4v ]; then
+        mv ~/apps/p4v ~/apps/p4v-baq
+    fi
 
-rm p4v.tgz 
+    if [ ! -d ~/apps ]; then
+        mkdir ~/apps/p4v
+    fi
 
-if [ -f /usr/local/bin/p4merge ]; then
-    sudo rm /usr/local/bin/p4merge
-fi
+    if [ ! -d ~/apps/p4v ]; then
+        mkdir ~/apps/p4v
+    fi
+}
 
-sudo ln -s $(realpath bin/p4merge) /usr/local/bin/p4merge
-popd
+create_symlink() {
+    if [ -f /usr/local/bin/p4merge ]; then
+        sudo rm /usr/local/bin/p4merge
+    fi
+
+    sudo ln -s $(realpath bin/p4merge) /usr/local/bin/p4merge
+}
+
+download_p4merge_archive() {
+    # TODO: instead of hard-coded url for a specific version
+    # it would be best to get info on the newest version availablev
+    wget -O p4v.tgz http://filehost.perforce.com/perforce/r18.2/bin.linux26x86_64/p4v.tgz
+}
+
+extract_p4merge() {
+    tar -xf p4v.tgz --strip 1
+    rm p4v.tgz 
+}
+
+install
