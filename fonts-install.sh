0d02c4fb2bf89cc0faa3ac6edae9a953c6223eec
MaciekTalaska <maciek.talaska@gmail.com>
2019-01-27 21:00:20 +0100

    fonts: install SourceCode font from github

--- fonts-install.sh
+++ fonts-install.sh
@@ -1,11 +1,35 @@
-pushd
+#! /usr/bin/env bash
+
+pushd pwd
 mkdir ~/config
 cd ~/config
 
+echo "downloading powerline fonts"
 git clone https://github.com/powerline/fonts powerline-fonts
 cd powerline-fonts
-./install deja
+./install.sh Deja
 
 cd ..
 rm powerline-fonts -rf
 
+# part of the script below is based on the following gist:
+# https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
+
+echo "installing SourceCodePro (book)"
+curl -s https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest \
+| grep "SourceCodeVariable-Roman.otf" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+
+echo "installing SourceCodePro (italic)"
+curl -s https://api.github.com/repos/adobe-fonts/source-code-pro/releases/latest \
+| grep "SourceCodeVariable-Italic.otf" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+mv SourceCodeVariable*.* ~/.local/share/fonts/
+
+popd
