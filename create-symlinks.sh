671ca093749459ccc1960c6a81567b620ace13fc
MaciekTalaska <maciek.talaska@gmail.com>
2019-02-13 16:44:08 +0100

    improved symlinks management

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,16 +3,40 @@
 user=$(whoami)
 home=/home/$user
 current_dir=$(pwd)
-#echo "home directory is: "
-#echo $home
-#echo "current directory: "
-#echo $current_dir
 
+files=('.bashrc' 
+        '.vimrc' 
+        './gitconfig' 
+        '.spacemacs' 
+        '.tmux.conf' 
+        '.Xresources' 
+        '.zprofile' 
+        '.zshrc')
 
- sudo ln -s $current_dir/.vimrc 	$home/.vimrc
- sudo ln -s $current_dir/.gitconfig 	$home/.gitconfig
- sudo ln -s $current_dir/.spacemacs 	$home/.spacemacs
- sudo ln -s $current_dir/.tmux.conf	$home/.tmux.conf
- sudo ln -s $current_dir/.Xresources	$home/.Xresources
- sudo ln -s $current_dir/.zprofile	$home/.zprofile
- sudo ln -s $current_dir/zshrc		$home/.zshrc
+for f in "${files[@]}"
+do
+    echo $f
+    echo $current_dir/$f
+    echo $home/$f
+done
+
+create_symlinks() {
+    for file in "${files[@]}"
+    do
+        sudo ln -s $current_dir/$file $home/$file
+    done
+}
+
+remove_symlinks() {
+    for file in "$files[@]"
+    do
+        rm $home/$file
+    done
+}
+
+if [ $1 = "-u" ];
+then
+    remove_symlinks
+else
+    create_symlinks
+fi
