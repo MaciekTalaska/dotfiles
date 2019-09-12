f4546ea84a9b3bb170034035bcbadff86b1f3e8d
MaciekTalaska <maciek.talaska@gmail.com>
2019-09-12 03:21:35 +0200

    asdf nodejs reinstall: exit if asdf errcode!=0, documentation update

--- node_package_reinstall.sh
+++ node_package_reinstall.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+# the idea of this script is to mimic the bahvior of the '--reinstall-packages-from' from the nvm
+#
+# what this script does is:
+# 1) switches to the version that has global packages installed (2nd script argument provided)
+# 2) retrieves the list of the packages installed 
+# 3) switches to the version of NodeJS that we want to install packages for (1st argument provided)
+# 4) installs all the global packages
+#
+# error codes used:
+# 1 - empty version (source or target) specified
+# 2 - unable to switch to source version (not installed? misspelled?)
+# 3 - unable to switch to target version (not installed? misspelled?)
+
 retrieve_packages_names() {
 	asdf global nodejs $1
+  if [ $? -ne 0 ]
+  then
+    echo ""
+    # error code doesn't matter at all, empty string will be returned anyway
+    return
+  fi
+
   packages=$(npm list -g --depth=0 --parseable)
   #packages=$(cat npm_packages.txt)
 	asdf_path=$(which asdf | sed 's:/bin/asdf::')
@@ -12,18 +32,25 @@ retrieve_packages_names() {
 
 install_packages() {
 	asdf global nodejs $1
-  npm install -g $2
+  if [ $? -ne 0 ]
+  then
+    exit 3 
+  fi
+
+  # by default npm is just too chatty...
+  npm install -g --silent $2
 }
 
 print_usage() {
-  echo "this script requires two NodeJS versions to be specified"
-  echo "the new version just recently installed (target)"
-  echo "and the old version, with some packages already installed (source)"
+  echo "Reinstall Node Packages From script"
+  echo ""
+  echo "Usage:"
+  echo "  node_package_reinstall <new version> <old version>"
   echo ""
   echo "Example of usage: "
-  echo "node_package_reinstall 12.10.0 10.16.3"
+  echo "  node_package_reinstall 12.10.0 10.16.3"
   echo ""
-  echo "this will install all global packages from 10.16.3 to 12.10.0"
+  echo "  (this installs all global packages from 10.16.3 to 12.10.0)"
 }
 
 check_for_arguments() {
@@ -40,8 +67,16 @@ check_for_arguments() {
   fi
 }
 
+# exit script if asdf or any other command return non 0 error code
+#set -e
 
 check_for_arguments $1 $2
 packages=$(retrieve_packages_names $2)
+#echo "content of packages: $packages"
+if [ -z "$packages" ]
+then
+  echo "error occurred. exiting..."
+  exit 2
+fi
 
 install_packages $1 "$packages"
