c8f4033a74c4354a77fd1ff4636295481e23bb76
MaciekTalaska <maciek.talaska@gmail.com>
2020-02-01 00:12:24 +0100

    fonts: install jetbrains mono

--- fonts-install.sh
+++ fonts-install.sh
@@ -1,5 +1,26 @@
 #! /usr/bin/env bash
 
+download_jetbrains_mono() {
+	#tag=$(curl -s https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+	tag=v1.0.2
+	echo "tag is: $tag"	
+	version="${tag:1}"	
+	echo "version is $version"
+	
+#  curl -s https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest \
+#  | grep "$1" \
+#  | cut -d : -f 2,3 \
+#  | tr -d \" \
+#  | wget --show-progress -qi -
+	# this is taken from: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8#gistcomment-2632806
+	#version=1.0.2
+  file=(JetBrainsMono-$version.zip)
+	unzip $file
+	mkdir -p ~/.local/share/fonts/jetbrainsmono
+	mv JetBrainsMono-$version/ttf/* ~/.local/share/fonts/jetbrainsmono
+	rm JetBrainsMono-$version.zip
+}
+
 # parameter is source file name
 download_sourcecodepro() {
   # part of the code below is based on the following gist:
@@ -38,6 +59,7 @@ cd ~/config
 
 install_powerline_fonts
 download_all_sourcecodepro_fonts
+download_jetbrains_mono
 
 popd >> /dev/null
 
