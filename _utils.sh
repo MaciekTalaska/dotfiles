77d5c12003cb8de256ad3740e8b673c66960264a
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-20 18:32:47 +0200

    _utils: additional functions

--- _utils.sh
+++ _utils.sh
@@ -1,8 +1,38 @@
 #! /usr/bin/env bash
 
+# require_exec - this function checks if specified executable 
+# is available for the current user on the system
+# if not - exits script immediatelly
+# argument: executable name
+require_exec() {
+  if ! bash _utils.sh file_in_path "$1"; then
+    echo "$1 does not exist and is required. aborting script..."
+    exit 1;
+  fi
+}
+
 clean_font_cache() {
   echo "clean font cache... (this may take a while)"
   fc-cache -f
 }
+
+# file_in_path - checks if specified executable
+# is available for the current user on the system
+# argument: executable name
+file_in_path() {
+  if command -v $1 >/dev/null 2>&1; then
+    return 0 
+  else 
+    return 1
+  fi
+}
+
+# get info on the latest version available in github repository
+# argument: json document from github repo
+get_latest_version_from_repo() {
+  version=$(echo "$1" | jq -r '.tag_name')
+  echo "$version"
+}
+
 
 "$@"
