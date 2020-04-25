36f39161a6970d88307059517c517572f199202d
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-25 20:23:01 +0200

    git-radar: quiet git clone

--- git-radar-install.sh
+++ git-radar-install.sh
@@ -6,15 +6,15 @@ MYBIN=$HOME/mybin
 source _utils.sh
 
 mkdir -p $CONFIG_DIR
-git clone https://github.com/michaeldfallen/git-radar $CONFIG_DIR/git-radar
+git clone https://github.com/michaeldfallen/git-radar $CONFIG_DIR/git-radar -q
 
 
 if [ ! -d $MYBIN ]; then
     echo "$MYBIN does NOT exist. creating & adding to PATH"
     source _utils.sh
-    create_directory_and_add_to_path $MYBIN 
+    create_directory_and_add_to_path "$MYBIN" 
 fi
 
-echo "make git-radar accessible for the current user"
+echo "git-radar installed."
 ln -s $CONFIG_DIR/git-radar/git-radar $MYBIN/git-radar  
 
