04db426328504b126c520e159ee688c1dc9cc85f
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-25 14:15:19 +0200

    git-radar: clone and create symlink in /home/emgieha/mybin

--- git-radar-install.sh
+++ git-radar-install.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/env bash
 
 CONFIG_DIR=$HOME/configuration
+MYBIN=$HOME/mybin
 
+source _utils.sh
+
+mkdir -p $CONFIG_DIR
 git clone https://github.com/michaeldfallen/git-radar $CONFIG_DIR/git-radar
+
+
+if [ ! -d $MYBIN ]; then
+    echo "$MYBIN does NOT exist. creating & adding to PATH"
+    create_directory_and_add_to_path $MYBIN 
+    ln -s $CONFIG_DIR/git-radar/git-radar $MYBIN/git-radar  
+fi
+
